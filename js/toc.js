daba7a5fa2b3128191a48f46e0b8a1b20293b9cd
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-07-31 19:17:14 -0400

    update analyses page with two tabs

--- js/toc.js
+++ js/toc.js
@@ -1,25 +1,32 @@
 (function () {
-    var toc = '<h3 class="ui header">TOC</h3>';
-    toc += "<div class='ui large bulleted list'>";
-    var ind = false;
-    var active_toc = $('.ui.active.tab .ui.grid .ui.text').attr('id');
-    active_toc = '#' + active_toc + ' h1';
-    console.log(active_toc);
-    $(active_toc).each(function(){
-        current = $(this);
-        if(ind){
-            var html = current.html();
-            var html_new = html.substring(html.indexOf('_'), html.indexOf('>')-1);
-            html = current.text();
-            html_new = "<a href='#" + html_new + "'>" + html + "</a>";
-        }
-        else{
-            var html = current.text();
-            var html_new = "<a href='#_top'>" + html + "</a>";
-            ind = true;
-        }
-        toc += "<a class='item'>" + html_new + "</a>";
+    function setTOC(curr_toc){
+        var toc = '<h3 class="ui header">TOC</h3>';
+        toc += "<div class='ui large bulleted list'>";
+        var ind = false;
+        var toc_name = '#' + curr_toc + ' h1';
+        var sticky_name = '#' + curr_toc + '_sticky';
+        $(toc_name).each(function(){
+            current = $(this);
+            if(ind){
+                var html = current.html();
+                var html_new = html.substring(html.indexOf('_'), html.indexOf('>')-1);
+                html = current.text();
+                html_new = "<a href='#" + html_new + "'>" + html + "</a>";
+            }
+            else{
+                var html = current.text();
+                var html_new = "<a href='#_top'>" + html + "</a>";
+                ind = true;
+            }
+            toc += "<a class='item'>" + html_new + "</a>";
+        });
+        toc += "</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+        $(sticky_name).html(toc);
+    };
+
+    $.each($('.ui.tab .ui.grid .ui.text'), function(index, item){
+        curr_toc = $(this).attr('id');
+        setTOC(curr_toc);
     });
-    toc += "</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
-    $(".ui.sticky").html(toc);
+
 })();
