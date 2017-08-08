4a5a9ae9f95286dea815f7449c4dc53c6e1ca0c3
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-08 19:22:54 -0400

    --allow-empty

--- routes/data.js
+++ routes/data.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var itemRouter = express.Router({mergeParams: true});
-
 /* GET home page. */
-
-router.use('/:data')
 
 
 router.get('/', function(req, res, next) {
-  res.render('partials/_data_collection', {subject:'Hello!'});
-  res.render('data', { title: 'Datasets' , active:{ data: true}});
-  console.log('I am here!');
+  res.render('data', {
+    title: 'Datasets' ,
+    active: {data: true},
+    collection: {
+      title: "Hello!!!!!"
+    }
+    });
 });
 
 module.exports = router;
