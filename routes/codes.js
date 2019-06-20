7853465db021f80ab7c33642882eed2c2d74f365
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-06-20 17:15:56 -0400

    send new code request to git repo issue

--- routes/codes.js
+++ routes/codes.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
+var createIssue = require( 'github-create-issue');
 
 // var url = 'mongodb://druckmannAdmin:Neural!dbadmin@localhost/druckmann_methodology?authSource=admin';
 var url = 'mongodb://localhost:27017/ephys_imaging_datasets';
 // var url = 'mongodb://mongo/ephys_imaging_datasets';
 var MongoClient = mongodb.MongoClient;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,10 +24,44 @@ router.post('/addmodel', function(req, res) {
     else{
       var collection = db.collection('n_models');
       collection.insert(req.body, function(err, result){
-        res.send((err === null) ? { msg: ''} : {msg: err});
+        if(err){
+          res.send(err);
+        }
+        else{
+          var user = req.body.firstname + ' ' + req.body.lastname;
+          var title = 'New codes from ' + user;
+          var email = req.body.email;
+          var repo = req.body.repo;
+          var ref = req.body.ref;
+          var code = req.body.code_type;
+          var body_text = 'User name: '+user + '<br> Email: ' + email + '<br> Code type:' + code + '<br> Repo: ' + repo;
+          var opts = {
+              'token': 'ccc8f9dc11b3bdc71bb219ef0a43a2542f13c9db',
+              'useragent': 'imphys-issuer',
+              'labels': ['New codes'],
+              'body': body_text
+          };
+          createIssue( 'zqwei/Im-phys-API', title, opts, clbk );
+        }
       });
     }
   });
 });
+
+
+function clbk(error, issue, info) {
+    // Check for rate limit information...
+    if ( info ) {
+        console.error( 'Limit: %d', info.limit );
+        console.error( 'Remaining: %d', info.remaining );
+        console.error( 'Reset: %s', (new Date( info.reset*1000 )).toISOString() );
+    }
+    if ( error ) {
+        throw new Error( error.message );
+    }
+    console.log( JSON.stringify( issue ) );
+    // returns <issue_data>
+}
+
 
 module.exports = router;
