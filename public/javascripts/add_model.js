1ecfb59d01f26b265545cda7d10212cfaa7acb7b
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-06-20 15:16:20 -0400

    add form validation

--- public/javascripts/add_model.js
+++ public/javascripts/add_model.js
@@ -1,18 +1,45 @@
+// obtain form values
 function getFieldValue(fieldId) {
    // 'get field' is part of Semantics form behavior API
    return $('.ui.form').form('get field', fieldId).val();
 }
 
+function getFormData(){
+  var formData = {
+            firstname: getFieldValue('first-name'),
+            lastname: getFieldValue('last-name'),
+            institution: getFieldValue('Institution'),
+            email: getFieldValue('email'),
+            code_type: getFieldValue('code_type'),
+            repo: getFieldValue('repo'),
+            ref: getFieldValue('reference'),
+            info : getFieldValue('info')
+        };
+  return formData;
+}
 
-var formData = {
-          firstname: getFieldValue('first-name'),
-          lastname: getFieldValue('last-name'),
-          institution: getFieldValue('Institution'),
-          email: getFieldValue('email'),
-          code_type: getFieldValue('code_type'),
-          repo: getFieldValue('repo'),
-          ref: getFieldValue('reference'),
-          info : getFieldValue('info')
-      };
+// form validation
+$('.ui.form').
+  form({
+      on: 'blur',
+      fields:{
+        firstname: {
+          identifier : 'first-name',
+          rules:[{type : 'empty'}]
+        },
+        lastname: {
+          identifier : 'last-name',
+          rules:[{type : 'empty'}]
+        },
+        repo: {
+          identifier : 'repo',
+          rules:[{type : 'empty'}]
+        },
+        email: {
+          identifier : 'email',
+          rules:[{type : 'email'}]
+        }
+      }
+    });
 
-alert(formData);
+$('.ui.form').form('validate form');
