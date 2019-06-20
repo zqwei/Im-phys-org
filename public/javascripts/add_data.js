3c2c4baed83068169c68005b16ace1b584ed3018
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-06-20 17:31:41 -0400

    start code for add new databases to data

---
+++ public/javascripts/add_data.js
@@ -0,0 +1,56 @@
+// obtain form values
+function getFieldValue(fieldId) {
+   // 'get field' is part of Semantics form behavior API
+   return $('.ui.form').form('get field', fieldId).val();
+}
+
+function getFormData(){
+  var formData = {
+            firstname: getFieldValue('first-name'),
+            lastname: getFieldValue('last-name'),
+            institution: getFieldValue('Institution'),
+            email: getFieldValue('email'),
+            code_type: getFieldValue('code_type'),
+            repo: getFieldValue('repo'),
+            ref: getFieldValue('reference'),
+            info : getFieldValue('info')
+        };
+  return formData;
+}
+
+// form validation
+$('.ui.form').
+  form({
+      on: 'blur',
+      fields:{
+        firstname: {
+          identifier : 'first-name',
+          rules:[{type : 'empty'}]
+        },
+        lastname: {
+          identifier : 'last-name',
+          rules:[{type : 'empty'}]
+        },
+        repo: {
+          identifier : 'repo',
+          rules:[{type : 'empty'}]
+        },
+        email: {
+          identifier : 'email',
+          rules:[{type : 'email'}]
+        }
+      }
+    });
+
+// submit form
+function submitForm(event){
+  event.preventDefault();
+  var formData = getFormData();
+  $.ajax({ type: 'POST', url: '/codes/addmodel', data: formData, dataType: 'JSON', success: onFormSubmitted });
+}
+
+function onFormSubmitted(res) {
+  alert('Thanks to submit your codes! We have added it in our database and also open an git issue at https://github.com/zqwei/Im-phys-API/issues.')
+}
+
+$('.ui.form').form('validate form',{onSuccess: submitForm(event)});
