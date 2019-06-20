e563303d9cb42dd09cd178bb38e99522fee139ec
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-06-20 14:21:39 -0400

    add code to get field data --  dropdown and textarea not working

--- public/javascripts/add_model.js
+++ public/javascripts/add_model.js
@@ -1,1 +1,18 @@
-alert('test test');
+function getFieldValue(fieldId) {
+   // 'get field' is part of Semantics form behavior API
+   return $('.ui.form').form('get field', fieldId).val();
+}
+
+
+var formData = {
+          firstname: getFieldValue('first-name'),
+          lastname: getFieldValue('last-name'),
+          institution: getFieldValue('Institution'),
+          email: getFieldValue('email'),
+          code_type: getFieldValue('code_type'),
+          repo: getFieldValue('repo'),
+          ref: getFieldValue('reference'),
+          info : getFieldValue('info')
+      };
+
+alert(formData);
