826b4deb01c477c97f36d77720f7553fec40325f
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-07-07 23:50:52 -0400

    update data collection tab

--- public/javascripts/datasetList.js
+++ public/javascripts/datasetList.js
@@ -24,8 +24,9 @@ $.get( '/data/senddata/', function(data, status){
     behavior_ = parseInt(behavior_);    
     var data_info = collection[collect_]['behavior'][behavior_];
     var title = collection[collect_]["brain_area"] + '  --  ' + data_info["name"]; 
-    html = '<h3>'+title+'</h3>'+'<span class="ui blue label">'+data_info['label']+'</span>';
-    var content = '<div class="ui accordion">'+data_info['description']+'<div class="title"><i class="dropdown icon"></i> Read more </div> <div class="content">'+data_info['description_detail']+'<br><br><img src="'+data_info['description_images']+'" height="150" class="ui centered image"><br><br></div></div></div>';
+    html = '<h3>'+title+'</h3>';
+    // html += '<span class="ui blue label">'+data_info['label']+'</span>';
+    var content = '<div class="ui accordion">'+data_info['description']+'<div class="title"><i class="dropdown icon"></i> Detailed task description </div> <div class="content">'+data_info['description_detail']+'<br><br><img src="'+data_info['description_images']+'" height="150" class="ui centered image"><br><br></div></div></div>';
     
     var list_item = '';
     $.each(objectArrary, function(ind, value){
@@ -49,26 +50,13 @@ function subdataHtml(info, name){
   $.each(info, function(ind, value){
     if (this.name===name){
       html_ = '<li><h4>'+name+'</h4></li>';
-      html_ += '<div class="ui accordion"><div class="title">  Description <i class="dropdown icon"></i> </div> <div class="content">'+this['Description']+'<br></div></div>';
+//       html_ += '<div class="ui accordion"><div class="title">  Task description <i class="dropdown icon"></i> </div> <div class="content">'+this['Description']+'<br></div></div>';
+      html_ += '<span class="ui small gray label">Sample size</span>' + 'x animals';
+      html_ += 'x sessions';
+      html_ += 'x neurons' + '<br>';
       html_ += '<span class="ui small gray label">Reference</span>' +this['Reference'][0] + '<br>';
       html_ += '<span class="ui small gray label">Link</span><a href="'+this['Linkhref']+'">'+this['Link']+'</a><br>';
     }
   });
   return html_;
 }
-
-//     {{#each OtherLabel}}
-//       <span class="ui gray label">{{this}}</span>
-//     {{/each}}
-//     <br> {{Description}}
-//     <br> <br> <div class="ui gray label">
-//       References
-//     </div>
-//     <ul class="ui list">
-//       {{#each Reference}}
-//         <li>{{this}}</li>
-//       {{/each}}
-//     </ul>
-//     <span class="ui gray label">Link</span> <a href="{{Linkhref}}">{{Link}}</a>
-//     <br>
-//     <br>
